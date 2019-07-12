d9e6c8ca791e78834378a362457e9a0eea66a320
JesseRautiainen <jesse.rautiainen@turku.fi>
2019-07-12 13:08:29 +0300

    added mobile navbar tests

---
+++ app/shared/top-navbar/mobile/MobileNavbar.spec.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Navitem from 'react-bootstrap/lib/NavItem';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
+import { exportAllDeclaration } from '@babel/types';
+
+import { shallowWithIntl } from 'utils/testUtils';
+import MobileNavbar from './MobileNavbar';
+import TopNavbarContrastContainer from '../accessability/TopNavbarContrastContainer';
+import TopNavbarFontContainer from '../accessability/TopNavbarFontContainer';
+
+
+describe('shared/top-navbar/mobile/MobileNavbar', () => {
+  function getWrapper(props) {
+    const defaults = {
+      changeLocale: () => null,
+      currentLanguage: 'fi',
+      isLoggedIn: false,
+      userName: 'Luke Skywalker',
+    };
+    return shallowWithIntl(<MobileNavbar {...defaults} {...props} />);
+  }
+
+  test('render contrastChanger', () => {
+    const element = getWrapper().find(TopNavbarContrastContainer);
+    expect(element.length).toBe(1);
+  });
+
+  test('render fontChanger', () => {
+    const element = getWrapper().find(TopNavbarFontContainer);
+    expect(element.length).toBe(1);
+  });
+});
