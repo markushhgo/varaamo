0d401d2b8696b9705aa69ae4a36c5c4c033f72a3
Jarno <jarno.vuorela@turku.fi>
2019-08-09 10:02:09 +0300

    Repositioned address texts and made links seperate.

--- app/pages/resource/resource-info/ResourceInfo.js
+++ app/pages/resource/resource-info/ResourceInfo.js
@@ -27,9 +27,9 @@ function ResourceInfo({
         <Panel defaultExpanded header={t('ResourceInfo.additionalInfoTitle')}>
           <Row>
             <Col className="app-ResourceInfo__address" lg={6} md={6} sm={6} xs={12}>
-              {unit && unit.name && <span>{unit.name}</span>}
+              {unit && unit.name && <span2>{unit.name}</span2>}
               {unit && unit.streetAddress && <span>{unit.streetAddress}</span>}
-              {unit && <span>{`${unit.addressZip} ${upperFirst(unit.municipality)}`.trim()}</span>}
+              {unit && <span3>{`${unit.addressZip} ${upperFirst(unit.municipality)}`.trim()}</span3>}
             </Col>
             <Col className="app-ResourceInfo__web" lg={6} md={6} sm={6} xs={12}>
               {serviceMapUrl && (
