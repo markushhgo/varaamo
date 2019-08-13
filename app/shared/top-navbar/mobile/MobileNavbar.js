97d52153b86f73f6cb49e4d373cf30589ded97a5
Hannes <50320115+hienous@users.noreply.github.com>
2019-08-13 14:25:16 +0300

    added tests & code refactor
    Refactored mobilenavbar and added tests

--- app/shared/top-navbar/mobile/MobileNavbar.js
+++ app/shared/top-navbar/mobile/MobileNavbar.js
@@ -15,29 +15,16 @@ class MobileNavbar extends React.Component {
 
   getCollapseStatus = () => (this.props.toggle ? '' : 'is-collapsed')
 
-  getAriaVisibility = () => (!this.props.toggle)
-
-  getFontChanger() {
-    return <FontChanger />;
-  }
-
-  getContrastChanger() {
-    return <ContrastChanger />;
-  }
-
   render() {
     const element = this.getCollapseStatus();
-    const ariaHide = this.getAriaVisibility();
-    const fontComponent = this.getFontChanger();
-    const contrastComponent = this.getContrastChanger();
     return (
-      <div aria-hidden={ariaHide} aria-live="polite" className={classNames('mobile-Navbar_mobile', element, this.props.contrast)} id="mobileNavbar">
+      <div aria-hidden={!this.props.toggle} aria-live="polite" className={classNames('mobile-Navbar_mobile', element, this.props.contrast)} id="mobileNavbar">
         <div className="container">
           <Row>
             <Col sm={6} smOffset={6} xs={12}>
               <ul>
-                {contrastComponent}
-                {fontComponent}
+                <ContrastChanger />
+                <FontChanger />
               </ul>
             </Col>
           </Row>
--- app/shared/top-navbar/mobile/MobileNavbar.spec.js
+++ app/shared/top-navbar/mobile/MobileNavbar.spec.js
@@ -27,4 +27,29 @@ describe('shared/top-navbar/mobile/MobileNavbar', () => {
     const element = getWrapper().find(TopNavbarFontContainer);
     expect(element.length).toBe(1);
   });
+
+  describe('div.mobile-nav ', () => {
+    const prop = {
+      toggle: false,
+      contrast: '',
+    };
+
+    test('has correct props', () => {
+      const element = getWrapper({ prop }).find('div').first();
+      expect(element.prop('aria-hidden')).toBe(true);
+      expect(element.hasClass('is-collapsed')).toBe(true);
+      expect(element.hasClass('high-contrast')).toBe(false);
+    });
+
+    test('has correct prop when isHighContrast: true', () => {
+      const element = getWrapper({ contrast: 'high-contrast' }).find('div').first();
+      expect(element.hasClass('high-contrast')).toBe(true);
+    });
+
+    test('has correct prop when toggle: true', () => {
+      const element = getWrapper({ toggle: true }).find('div').first();
+      expect(element.prop('aria-hidden')).toBe(false);
+      expect(element.hasClass('is-collapsed')).toBe(false);
+    });
+  });
 });
