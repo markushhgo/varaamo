b47bd45a3cc3110892502728494cbeb63c8c58cc
Santtu Alatalo <santtualatalo@gmail.com>
2019-09-19 13:38:14 +0300

    Changed resource map service links to point to Turku's map service.

--- app/utils/__tests__/unitUtils.spec.js
+++ app/utils/__tests__/unitUtils.spec.js
@@ -4,11 +4,16 @@ import {
 
 describe('Utils: unitUtils', () => {
   describe('getServiceMapUrl', () => {
-    test('returns correct url', () => {
-      const unit = { id: 'abc:123' };
-      const expected = 'https://palvelukartta.hel.fi/unit/123#!route-details';
+    test('returns correct url when unit has mapServiceId defined', () => {
+      const unit = { mapServiceId: '123' };
+      const expected = 'https://palvelukartta.turku.fi/unit/123#!route-details';
 
       expect(getServiceMapUrl(unit)).toBe(expected);
+    });
+
+    test('returns empty string when unit or unit.mapServiceId is missing', () => {
+      const unit = { mapServiceId: null };
+      expect(getServiceMapUrl(unit)).toBe('');
     });
   });
 });
