ae2eb94e270e46d7e6b96950d986607a3a9e1376
Tino Roivanen <50320109+Tinosaurus@users.noreply.github.com>
2019-11-08 10:19:43 +0200

    Fix/var85 recurring reservation date selector (#37)
    * Improvements to form layout and appearance. Calendar no longer overflows on desktop and tablets
    
    * Calendar element fixes on mobile.

--- app/shared/recurring-reservation-controls/RecurringReservationControls.js
+++ app/shared/recurring-reservation-controls/RecurringReservationControls.js
@@ -44,7 +44,7 @@ function RecurringReservationControls({
   return (
     <div className="recurring-reservation-controls">
       <Row>
-        <Col sm={5} xs={12}>
+        <Col sm={6} xs={12}>
           <div className="recurring-reservation-frequency-control">
             <label htmlFor="recurrence-frequency-select">
               {t('RecurringReservationControls.frequencyLabel')}
@@ -62,7 +62,7 @@ function RecurringReservationControls({
           </div>
         </Col>
         {frequency !== '' && (
-          <Col sm={3} xs={12}>
+          <Col sm={6} xs={12}>
             <FormGroup controlId="numberOfOccurrencesGroup">
               <ControlLabel>
                 {t('RecurringReservationControls.numberOfOccurrencesLabel')}
@@ -77,7 +77,7 @@ function RecurringReservationControls({
           </Col>
         )}
         {frequency !== '' && (
-          <Col sm={4} xs={12}>
+          <Col sm={12} xs={12}>
             <FormGroup controlId="LastTimeGroup">
               <ControlLabel>{t('RecurringReservationControls.lastTimeLabel')}</ControlLabel>
               <DatePicker
