4cda29a1db47ab2242aee7cffe0912d73a51ed3e
Tino Roivanen <tino.roivanen98@gmail.com>
2019-11-15 10:21:56 +0200

    Remote implementation of matomo.

--- server/Html.js
+++ server/Html.js
@@ -19,7 +19,7 @@ class Html extends Component {
       _paq.push(['trackPageView']);
       _paq.push(['enableLinkTracking']);
       (function() {
-        var u="//localhost/matomo/";
+        var u="//testivaraamo.turku.fi:8003/matomo/";
         _paq.push(['setTrackerUrl', u+'piwik.php']);
         _paq.push(['setSiteId', ${piwikSiteId}]);
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
@@ -30,7 +30,7 @@ class Html extends Component {
         s.parentNode.insertBefore(g,s);
       })();
     `;
-    const imgSrc = `//localhost/matomo/piwik.php?idsite=${piwikSiteId}`;
+    const imgSrc = `//testivaraamo.turku.fi:8003/matomo/piwik.php?idsite=${piwikSiteId}`;
     return (
       <div>
         <script dangerouslySetInnerHTML={{ __html: scriptString }} />
