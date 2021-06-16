0244eb340d78b61eb4857012c79cc46ff110c215
Santtu Alatalo <santtualatalo@gmail.com>
2021-06-16 07:37:53 +0300

    Updated browser warning texts (#135)

--- app/pages/browser-warning/BrowserWarning.js
+++ app/pages/browser-warning/BrowserWarning.js
@@ -4,60 +4,33 @@ function BrowserWarning() {
   return (
     <div>
       <p className="alert alert-warning">
-
-
-        Currently, Varaamo does not support Internet Explorer.
-        We are investigating this issue and finding a solution.
-        Meanwhile, please use another browser (such as
+        Varaamo does not support Internet Explorer.
+        Please use another browser (such as
         <a href="https://www.google.com/chrome/"> Chrome</a>
-
-
         ,
         <a href="https://www.mozilla.org/en-US/firefox/new/"> Firefox </a>
-
-
         or
         <a href="https://www.microsoft.com/en-us/windows/microsoft-edge"> Edge</a>
-
-
         ).
       </p>
       <p className="alert alert-warning">
-
-
-        Varaamo ei tue Internet Explorer selainta tällä hetkellä.
-        Selvitämme ongelmaa sen ratkaisemiseksi.
-        Sillä välin, käytä toista selainta (kuten
+        Varaamo ei tue Internet Explorer selainta.
+        Käytä toista selainta (kuten
         <a href="https://www.google.com/chrome/"> Chrome</a>
-
-
         ,
         <a href="https://www.mozilla.org/en-US/firefox/new/"> Firefox </a>
-
-
         tai
         <a href="https://www.microsoft.com/en-us/windows/microsoft-edge"> Edge</a>
-
-
         ).
       </p>
       <p className="alert alert-warning">
-
-
-        Varaamo fungerar inte längre med Internet Explorer.
-        Vi arbetar med att lösa problemet.
-        Under tiden så var vänlig och använd någon annan browser (t.ex
+        Varaamo stöder inte Internet Explorer.
+        Vänligen använd någon annan webbläsare (t.ex.
         <a href="https://www.google.com/chrome/"> Chrome</a>
-
-
         ,
         <a href="https://www.mozilla.org/en-US/firefox/new/"> Firefox </a>
-
-
         eller
         <a href="https://www.microsoft.com/en-us/windows/microsoft-edge"> Edge</a>
-
-
         ).
       </p>
     </div>
